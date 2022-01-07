95b9680c52f459a97e8dd85db9d1a3716b82890d
Caleb <Caleb@qq.com>
2022-01-07 16:30:31 +0800

    first commit

---
+++ src/main/java/com/study/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.study.git;
+
+/**
+ * @author Clark
+ * @create 2022-01-07 16:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
